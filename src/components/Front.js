53bd9dc5f96b6012c4317b2af87f8f60d9f61394
mrjasonweaver <indyplanets@gmail.com>
2017-10-21 13:40:01 -0500

    setup initial front app component

---
+++ src/components/Front.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Front extends React.Component {
+    render() {
+        return (
+            <p>Welcome to Workout Randomizer!</p>
+        )
+    }
+}
+
+export default Front;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { render } from 'react-dom';
+import './css/style.css';
+import Front from './components/Front';
+
+render(<Front/>, document.querySelector('#main'));
