a85921dd8f74e3d654ea9b062cf9492683cc2555
mrjasonweaver <indyplanets@gmail.com>
2017-10-21 23:35:56 -0500

    Add workout form

--- src/components/AddWorkout.js
+++ src/components/AddWorkout.js
@@ -7,7 +7,8 @@ class AddWorkout extends React.Component {
                 <div className="wrap">
                     <form className="add-workout">
                         <h3>{this.props.sectionTitle}</h3>
-                        
+                        <input placeholder="workout Name"/><button>Add</button>
+                        <input hidden placeholder="workout item"/><button hidden>Add</button>
                     </form>
                     <p>List workouts here</p>
                 </div>
