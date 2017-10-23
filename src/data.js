f17d34b1a0448cf9b4a86d192f09e2db74cd19d3
mrjasonweaver <indyplanets@gmail.com>
2017-10-22 19:27:57 -0500

    Adding seed data

---
+++ src/data.js
@@ -0,0 +1,45 @@
+const workouts = [
+    {
+        "workout_id": 1,
+        "date": "2017-04-23T00:00:00.511Z",
+        "workout_name": "Back and core",
+        "workout_items": [
+            "Run 1 mile",
+            "Stretch",
+            "Deadlifts 3x12",
+            "Pushups 3x12",
+            "Crunches 3x30",
+        ],
+        "is_saved": false
+    },
+    {
+        "workout_id": 2,
+        "date": "2017-06-13T01:25:43.511Z",
+        "workout_name": "Legs and shoulders",
+        "workout_items": [
+            "Jumping Jacks x 100",
+            "Stretch",
+            "Front Squats 3x12",
+            "Military Dumbbell Press 3x12",
+            "Dumbbell Shoulder Raises 3x12",
+
+        ],
+        "is_saved": true
+    },
+    {
+        "workout_id": 3,
+        "date": "2017-10-10T01:30:43.511Z",
+        "workout_name": "Power Moves",
+        "workout_items": [
+            "High Knee Skips x 30",
+            "Stretch",
+            "Power Cleans 3x12",
+            "Bent over rows 3x12",
+            "Side-to-side leaps 3x20",
+
+        ],
+        "is_saved": true
+    }
+]
+
+export default workouts;
